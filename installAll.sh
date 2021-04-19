144bf97541611fe67a0c60b4e425b3b9c6914c2a
andersonrev <fabiander_21@hotmail.es>
2021-04-19 10:32:26 -0500

    installadores

---
+++ installAll.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+sudo snap install webstorm --classic
+
+sudo snap install gitkraken --classic
+sudo snap install spotify
+sudo snap install discord --classic
+sudo snap install dbeaver-ce
+
+
+sudo apt-get install \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    gnupg \
+    lsb-release
+
+
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+
+echo \
+  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io
+curl https://packages.microsoft.com/keys/microsoft.asc | sudo apt-key add -
+
+sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/ms-teams stable main" > /etc/apt/sources.list.d/teams.list'
+
+sudo apt update
+
+sudo apt install teams
+
+sudo apt install neovim
+
